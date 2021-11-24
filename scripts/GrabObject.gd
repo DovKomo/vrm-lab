96c6b21fe17df671e1fae4296448a27d96dcec01
Dovile <dovile.komolovaite@gmail.com>
2021-11-24 21:53:17 +0200

    scripting

---
+++ scripts/GrabObject.gd
@@ -0,0 +1,48 @@
+extends Area
+
+var grabbable_objects: Array
+var grabbed_object: RigidBody  # if not Null, then we have something in our hand
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func attempt_grab():
+	if grabbable_objects.size() == 0:
+		return
+		
+	var min_dist = 9999.99
+	var potential_obj  = null
+	
+	for obj in grabbable_objects:
+		# look for the smallest distance between the hand and an object:
+		var dist = global_transform.origin.distance_to(obj.global_transform.origin)
+		if dist < min_dist:
+			min_dist = dist
+			potential_obj = obj
+	add_child(potential_obj)
+	grabbed_object = potential_obj
+	grabbed_object.mode = RigidBody.MODE_KINEMATIC # stop being effected by external forces
+	
+func attempt_releace():
+	if grabbed_object == null: # nothing to releace
+		return
+		
+	remove_child(grabbed_object) # we should put it in root
+	grabbed_object.mode = RigidBody.MODE_RIGID # be controled by phisics engine again
+	grabbed_object = null
+	
+
+func _on_HandOverlap_body_entered(body):
+	if body is RigidBody:
+		grabbable_objects.append(body)
+
+
+func _on_HandOverlap_body_exited(body):
+	if grabbable_objects.find(body) != 1: 
+		grabbable_objects.erase(body)
+		
+	
+func is_grabbing():
+	return grabbed_object != null
